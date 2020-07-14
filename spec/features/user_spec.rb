7e27f0447fddc56c4666e0180750d8aacedc719e
Jane Greene <janegreenecasa@gmail.com>
2020-07-14 09:22:26 -0600

    writing test

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "as a user", :vcr do
+  it "can search for members" do
+    visit root_path
+
+    # Then /^"([^"]*)" should contain "([^"]*)"$/ do |dropdown, text|
+    #   expect(page).to have_select(dropdown, :options => [text])
+    # end
+
+    click_on "Search For Members"
+    expect(current_path).to eq(search_path)
+    
+  end
+end
+
+# As a user,
+# When I visit "/"
+# And I Select "Fire Nation" from the select field
+# (Note: Use the existing select field)
+# And I click "Search For Members“
+# Then I should be on page “/search”
+# Then I should see the total number of people who live in the Fire Nation. (i.e. 20)
+# And I should see a list with the detailed information for the 20 members of the Fire Nation.
+#
+# And for each of the members I should see:
+# - The name of the member (and their photo, if they have one)
+# - The list of allies and a list of enemies
+# - Any affiliation that the member has
