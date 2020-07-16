7291986c153c004b81d02d5339a51a5578c0f1f6
Jane Greene <janegreenecasa@gmail.com>
2020-07-16 16:51:59 -0600

    Rolled back the db to remove characters table

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200714160437) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "characters", force: :cascade do |t|
-    t.string "allies"
-    t.string "enemies"
-    t.string "photoUrl"
-    t.string "name"
-    t.string "affiliation"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
