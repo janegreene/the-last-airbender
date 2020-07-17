72d2cea73bc7571a504789766a151f58a8462bf6
Jane Greene <janegreenecasa@gmail.com>
2020-07-16 18:23:27 -0600

    Refactored but still failing model tests need allies to be array

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,17 +1,15 @@
 
 class SearchController < ApplicationController
   def index
-    nation = params[:nation]
-
-    conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com/")do |faraday|
-      faraday.params["affiliation"] = 'nation'
-    end
-
-    response = conn.get("api/v1/characters?affiliation=#{nation}.json")
-    json = JSON.parse(response.body, symbolize_names: true)
-    # require "pry"; binding.pry
-    @characters = json[:results].map do |character_data|
-      Character.new(character_data)
-    end
+    search_nation = params['nation'].gsub('_', '+')
+    conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com")#do |faraday|
+    #   faraday.params["affiliation"] = 'nation'
+    # end
+    #
+    response = conn.get("api/v1/characters?affiliation=#{search_nation}")
+    payload = JSON.parse(response.body, symbolize_names: true)
+    # # @characters = json[:results].map do |character_data|
+    # #   Character.new(character_data)
+    # end
   end
 end
